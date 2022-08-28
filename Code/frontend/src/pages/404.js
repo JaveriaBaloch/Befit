b57f6fe99e477bb4266831935e971b9cb7737432
Chrisoman <c.meyer@student.xu-university.de>
2022-08-28 21:27:56 +0200

    Added Bootstrap to the 404 Page

--- Code/frontend/src/pages/404.js
+++ Code/frontend/src/pages/404.js
@@ -4,11 +4,16 @@ import {useNavigate,Link} from "react-router-dom"
 
 function NotFound(){
     return(
-        <div>
-            <section>
-                <h1>404 - Page not found</h1>
-            </section>
-        </div>
+        <main>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="heading mx-auto">
+                        <h1 className="text-center mt-3 text-black">404 - Page not found</h1>
+                        <div className="mx-auto bg-black mb-5"/>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
 export default NotFound;
